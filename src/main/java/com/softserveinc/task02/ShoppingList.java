ae9c3fcc7bdd1c564d28566ea4a13b2d90f49d16
Maxim <maxshaptala@gmail.com>
2022-01-24 09:58:47 +0200

    Update ShoppingList.java

--- src/main/java/com/softserveinc/task02/ShoppingList.java
+++ src/main/java/com/softserveinc/task02/ShoppingList.java
@@ -6,6 +6,11 @@ public class ShoppingList {
     public void add(String product) {
         // TODO: Add the product to list
     }
+    
+    public int count() {
+        // TODO: Remove the line below and implement this method. Method should return size of items
+        throw new RuntimeException("Not implemented yet");
+    }
 
     public void delete(String product) {
         // TODO: delete all products from list
